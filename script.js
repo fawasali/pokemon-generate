b73fb3c9d76cc61f6481de8b0afafe05b4037af8
ali <68717503+fawasali@users.noreply.github.com>
2023-04-23 20:32:03 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,126 @@
+const typeColor = {
+    bug: "#26de81",
+  dragon: "#ffeaa7",
+  electric: "#fed330",
+  fairy: "#FF0069",
+  fighting: "#30336b",
+  fire: "#f0932b",
+  flying: "#81ecec",
+  grass: "#00b894",
+  ground: "#EFB549",
+  ghost: "#a55eea",
+  ice: "#74b9ff",
+  normal: "#95afc0",
+  poison: "#6c5ce7",
+  psychic: "#a29bfe",
+  rock: "#2d3436",
+  water: "#0190FF",
+};
+
+const url = "https://pokeapi.co/api/v2/pokemon/";
+const card= document.getElementById("card");
+const btn = document.getElementById("btn");
+
+let getPokeData = () => {
+    // generate a random nummber between 1 to 150
+    let id = Math.floor(Math.random() * 150) + 1;
+    //combine the poke api url with pokemon id
+    const finalUrl = url + id;
+    //fetch generate url
+    fetch(finalUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            generateCard(data);
+        }); 
+};
+
+//generate card
+
+let generateCard = (data) => {
+    // get necessary data and assign it to variable
+    console.log(data);
+    const hp = data.stats[0].base_stat;
+    console.log(hp);
+    const imgSrc = data.sprites.other.dream_world.front_default;
+    console.log(imgSrc);
+    const pokeName = data.name;
+    console.log(pokeName);
+    const statAttack = data.stats[1].base_stat;
+    console.log(statAttack);
+    const statDefense = data.stats[2].base_stat;
+    console.log(statDefense);
+    const statSpeed = data.stats[5].base_stat;
+    console.log(statSpeed);
+    // const typesType1 = data.types[0].type.name;
+    // console.log(typesType1);
+    // const typesType2 = data.types[1].type.name;
+    // console.log(typesType2);
+
+    const themeColor = typeColor[data.types[0].type.name];
+    console.log(themeColor);
+    
+
+    card.innerHTML = `
+    
+    <p class="hp">
+        <span>HP</span>
+        ${hp}
+    </p>
+    <img src="${imgSrc}" alt="fto">
+    <h2 class="poke-name">
+        ${pokeName}
+    </h2>
+    <div class="types">
+        
+    </div>
+    <div class="stats">
+        <div>
+            <h3>${statAttack}</h3>
+            <p>Attack</p>
+        </div>
+        <div>
+            <h3>${statDefense}</h3>
+            <p>Defense</p>
+        </div>
+        <div>
+            <h3>${statSpeed}</h3>
+            <p>Speed</p>
+        </div>
+    </div>
+    
+    `;
+    appendTypes(data.types);
+    styleCard(themeColor);
+    // styleBody(imgSrc);
+    
+};
+let appendTypes = (types) => {
+    console.log(types);
+    types.forEach((item) => {
+
+        let span = document.createElement("SPAN");
+        span.textContent = item.type.name;
+        // console.log(span);
+        document.querySelector(".types").appendChild(span);
+
+    });
+};
+
+let styleCard = (color) => {
+     card.style.background = `radial-gradient(circle at 50% 0%, ${color} 36%, #ffffff 36%)`;
+     card.querySelectorAll(".types span").forEach(typeColor => {
+        typeColor.style.backgroundColor = color
+     })
+};
+
+// let styleBody = (image) => {
+//     backgr.style.background = `url(${imgSrc})`;
+//     backgr.querySelectorAll("body").forEach(typeColor => {
+//         typeColor.style.background = image
+//     })
+// };
+
+
+
+btn.addEventListener("click", getPokeData);
+window.addEventListener("load", getPokeData)
